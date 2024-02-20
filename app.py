e9a9dbe080fe390a28a4e4a4125540c67cc5f06d
ssjx78 <anna.h.tuominen@durham.ac.uk>
2024-02-20 11:20:52 +0000

    Deploying a minor change to test functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Your Python App Example is running on AWH!"
+    return "Your Python App Example is running on AWH! Test"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
